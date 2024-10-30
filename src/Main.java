29a006281e10e161f334a9188dfea8d320233243
Yazfish <sergei_kuzmin@mail.ru>
2024-10-30 07:11:17 +0300

    HW comleted

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,86 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    // Методы
+    public static void fanny() {
+        System.out.println("____________________________________________________");
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static int calculateYear(int year) {
+        if (year > 1584 && ((year % 100 != 0 && year % 4 == 0) || year % 400 == 0)) {
+            return 1;
+        } else {
+            return 0;
         }
     }
+
+    public static int osUpdateMessage(int deviseYear, int ios) {
+        if (deviseYear >= 2015 && ios == 0) {
+            return 1;
+        } else if (deviseYear < 2015 && ios == 0) {
+            return 2;
+        } else if (deviseYear >= 2015 && ios == 1) {
+            return 3;
+        } else if (deviseYear < 2015 && ios == 1) {
+            return 4;
+        } else {
+            return 5;
+        }
+    }
+
+    public static int deliveryDays(int deliveryDist) {
+        if (deliveryDist <= 20) {
+            return 1;
+        } else if (deliveryDist > 20 && deliveryDist <= 60) {
+            return 2;
+        } else if (deliveryDist > 60 && deliveryDist <= 100) {
+            return 3;
+        } else {
+            return 4;
+        }
+    }
+
+    // код работы с методами
+    public static void main(String[] args) {
+        fanny();
+        System.out.println("Задача 1\n");
+        int currentYear = 2024;
+        int year = calculateYear(currentYear);
+        if (year > 0) {
+            System.out.println(currentYear + " год — високосный год.");
+        } else {
+            System.out.println(currentYear + " год — не високосный год.");
+        }
+        fanny();
+        System.out.println("Задача 2\n");
+        int clientDeviseYear = 2019;
+        int clientOs = 1;
+        int deviseYear = osUpdateMessage(clientDeviseYear, clientOs);
+        if (deviseYear == 1) {
+            System.out.println("Установите версию приложения для iOS по ссылке:");
+        } else if (deviseYear == 2) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке:");
+        } else if (deviseYear == 3) {
+            System.out.println("Установите версию приложения для Android по ссылке:");
+        } else if (deviseYear == 4) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке:");
+        } else {
+            System.out.println("Для вас нет обновлений");
+        }
+        fanny();
+        System.out.println("Задача 3\n");
+        int deliveryDistance = 55;
+        int deliveryTime = 1;
+        int deliveryDist = deliveryDays(deliveryDistance);
+        if (deliveryDist == 1) {
+            System.out.println("Потребуется дней: " + deliveryTime + ", для доставки вашей карты.");
+        } else if (deliveryDist == 2) {
+            System.out.println("Потребуется дней: " + (deliveryTime + 1) + ", для доставки вашей карты.");
+        } else if (deliveryDist == 3) {
+            System.out.println("Потребуется дней: " + (deliveryTime + 2) + ", для доставки вашей карты.");
+        } else {
+            System.out.println("На такое расстояние доставки нет.");
+        }
+        fanny();
+    }
+
 }
+
